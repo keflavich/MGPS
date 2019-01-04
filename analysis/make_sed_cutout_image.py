c8c4b958878c917033d1206f648a87b649cd94e5
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2019-01-04 14:27:13 -0700

    some pilot paper work

--- analysis/make_sed_cutout_image.py
+++ analysis/make_sed_cutout_image.py
@@ -50,7 +50,7 @@ def make_sed_plot(coordinate, mgpsfile, width=1*u.arcmin, surveys=Magpis.list_su
     coordname = "{0:06.3f}_{1:06.3f}".format(coordinate.galactic.l.deg,
                                              coordinate.galactic.b.deg)
 
-    mgps_cutout = Cutout2D(mgps_fh.data, coordinate, size=width, wcs=wcs.WCS(mgps_fh.header))
+    mgps_cutout = Cutout2D(mgps_fh.data, coordinate, size=width*2, wcs=wcs.WCS(mgps_fh.header))
     print(f"Retrieving MAGPIS data for {coordname} ({coordinate.to_string()} {coordinate.frame.name})")
     # we're treating 'width' as a radius elsewhere, here it's a full width
     images = {survey:getimg(coordinate, image_size=width*2.75, survey=survey) for survey in surveys}
@@ -85,7 +85,7 @@ def make_sed_plot(coordinate, mgpsfile, width=1*u.arcmin, surveys=Magpis.list_su
     # coordinate stuff so images can be reprojected to same frame
     ww = mgps_cutout.wcs.celestial
     target_header = ww.to_header()
-    pixscale = mgps_pixscale = (wcs.utils.proj_plane_pixel_area(ww)*u.deg**2)**0.5
+    mgps_pixscale = (wcs.utils.proj_plane_pixel_area(ww)*u.deg**2)**0.5
     target_header['NAXES'] = 2
     target_header['NAXIS1'] = target_header['NAXIS2'] = (width / mgps_pixscale).decompose().value
     #shape = [int((width / mgps_pixscale).decompose().value)]*2
@@ -95,12 +95,6 @@ def make_sed_plot(coordinate, mgpsfile, width=1*u.arcmin, surveys=Magpis.list_su
     figure.clf()
 
     for ii, (survey,img) in enumerate(images.items()):
-
-        #if hasattr(img[0], 'header'):
-        #    ww = wcs.WCS(img[0].header).celestial
-        #else:
-        #    ww = img[0].wcs
-        #pixscale = (wcs.utils.proj_plane_pixel_area(ww)*u.deg**2)**0.5
 
         if hasattr(img[0], 'header'):
             inwcs = wcs.WCS(img[0].header).celestial
@@ -124,14 +118,16 @@ def make_sed_plot(coordinate, mgpsfile, width=1*u.arcmin, surveys=Magpis.list_su
         else:
             new_img = img[0].data
             outwcs = img[0].wcs
-            pixscale_in = (wcs.utils.proj_plane_pixel_area(inwcs)*u.deg**2)**0.5
+            pixscale_in = (wcs.utils.proj_plane_pixel_area(outwcs)*u.deg**2)**0.5
 
         ax = figure.add_subplot(4, 5, ii+1, projection=outwcs)
         ax.set_title("{0}: {1}".format(survey, wlmap[survey]))
 
 
         norm = visualization.ImageNormalize(new_img,
-                                            interval=visualization.PercentileInterval(99.95))
+                                            interval=visualization.PercentileInterval(99.95),
+                                            stretch=visualization.AsinhStretch(),
+                                           )
 
         ax.imshow(new_img, origin='lower', interpolation='none', norm=norm)
         ax.set_xticklabels([])
@@ -147,5 +143,19 @@ def make_sed_plot(coordinate, mgpsfile, width=1*u.arcmin, surveys=Magpis.list_su
             xpix, ypix = outwcs.wcs_world2pix(coordinate.fk5.ra, coordinate.fk5.dec, 0)
         ax.set_xlim(xpix - (width/pixscale_in), xpix + (width/pixscale_in))
         ax.set_ylim(ypix - (width/pixscale_in), ypix + (width/pixscale_in))
+
+        # scalebar = 1 arcmin
+
+        ax.plot([xpix - width/pixscale_in + 5*u.arcsec/pixscale_in,
+                 xpix - width/pixscale_in + 65*u.arcsec/pixscale_in],
+                [ypix - width/pixscale_in + 5*u.arcsec/pixscale_in,
+                 ypix - width/pixscale_in + 5*u.arcsec/pixscale_in],
+                linestyle='-', linewidth=1, color='w')
+        ax.plot(crd_outframe.spherical.lon.deg, crd_outframe.spherical.lat.deg, marker=((0,-10), (0, -4)), color='w', linestyle='none',
+                markersize=20, markeredgewidth=0.5,
+                transform=ax.get_transform('world'))
+        ax.plot(crd_outframe.spherical.lon.deg, crd_outframe.spherical.lat.deg, marker=((4, 0), (10, 0)), color='w', linestyle='none',
+                markersize=20, markeredgewidth=0.5,
+                transform=ax.get_transform('world'))
 
     pl.tight_layout()
