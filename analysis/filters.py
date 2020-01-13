correct Ruze formula

analysis/filters.py
@@ -26,7 +26,7 @@ nu0 = 100*u.GHz
 def ruze(nu, epsilon=0.23025861*u.mm, eta_a=0.71):
     # 0.23 = 0.71 e^((-4 pi epsilon / 110 GHz in mm)^2) from Frayer+2018
     # epsilon = 0.23025861 mm = (-np.log(0.23 / 0.71) / (4*np.pi)**2 * (110*u.GHz).to(u.mm, u.spectral())**2) ** 0.5
-    return (eta_a * np.exp(-4*np.pi*epsilon / (nu.to(u.mm, u.spectral())))).decompose()
+    return (eta_a * np.exp(-(4*np.pi*epsilon / (nu.to(u.mm, u.spectral())))**2)).decompose()
 
 for alpha in np.arange(0, 4.5, 0.5):
     flux = (frq/nu0)**alpha
@@ -48,13 +48,13 @@ effective central frequencies
 4.0: 93.32862386559712 GHz
 
 with Ruze:
-    0.0 & 88.94 GHz & 3.371 mm
-0.5 & 89.39 GHz & 3.354 mm
-1.0 & 89.84 GHz & 3.337 mm
-1.5 & 90.30 GHz & 3.320 mm
-2.0 & 90.75 GHz & 3.303 mm
-2.5 & 91.21 GHz & 3.287 mm
-3.0 & 91.66 GHz & 3.271 mm
-3.5 & 92.11 GHz & 3.255 mm
-4.0 & 92.55 GHz & 3.239 mm
+0.0 & 88.37 GHz & 3.392 mm
+0.5 & 88.81 GHz & 3.376 mm
+1.0 & 89.26 GHz & 3.359 mm
+1.5 & 89.71 GHz & 3.342 mm
+2.0 & 90.16 GHz & 3.325 mm
+2.5 & 90.61 GHz & 3.309 mm
+3.0 & 91.06 GHz & 3.292 mm
+3.5 & 91.51 GHz & 3.276 mm
+4.0 & 91.95 GHz & 3.260 mm
 """